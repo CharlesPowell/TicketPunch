d985f73cce34cfffc13d7895c3f012f1613dd543
Charlie Powell <charliepowell1@gmail.com>
2015-05-19 16:33:08 -0500

    gets the app started

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 
   has_many :watch_list_entries
   has_many :movies, through: :watch_list_entries
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   def to_s
     self.username
